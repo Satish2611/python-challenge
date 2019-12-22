6bff601389ed5a04e7e1d2d6156bba8b34001efc
Satish Nirankari <nirankari.satish@gmail.com>
2019-12-21 19:24:28 -0600

    Final Check

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,34 +1,52 @@
+# Import os module,This will allow us to create file paths across operating systems
 import os
+# Module for reading CSV files
 import csv
-from itertools import tee
-
+#Import for converting date format
+import datetime
+#open csv 
 csvpath=os.path.join('Resources', 'budget_data.csv')
-
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+# skipping Header
     csv_header=next(csvreader)
-    csvreader1,csvreader2,csvreader3=tee(csvreader,3)
+#Setting up variable
     totalprofitloss=0
     lengthFile=0
     data=[]
-    for row in csvreader1:
+    month=[]
+#creating list by appending 
+    for row in csvreader:
         data.append(float(row[1]))
+        month.append(row[0])
     grtinc=0
     grtdec=0
+#Finding greatest increase and decrease
     for i in range(0,len(data)-1):
+#Greatest Increase
         if(data[i+1]-data[i]>grtinc):
             grtinc=data[i+1]-data[i]
+            monthinc=month[i+1]
+#Greatest decrease
         elif(data[i+1]-data[i])<grtdec:
             grtdec=data[i+1]-data[i]
-    totalprofit=sum(data)
+            monthdec=month[i+1]
+#Total profit
+    totalprofit=int(sum(data))
+#Average change
     firstrow=int(data[0])
     lastrow=int(data[-1])
     lengthrow=len(data)
     aveagechange=(lastrow-firstrow)/(lengthrow-1)
-    print(grtdec)
-    print(round(grtinc))
-    print(round(aveagechange,2))
-    print (lengthrow)
-    print (totalprofit)
-    
+#Changing Datetime format
+    montincformat=datetime.datetime.strptime(monthinc,'%b-%y')
+    montdecformat=datetime.datetime.strptime(monthdec,'%b-%y')
+#Displaying the output
+    print("Financial Analysis")
+    print("----------------------------")
+    print(f"Total Months:{lengthrow}")
+    print(f"Total: ${totalprofit}")
+    print(f"Average  Change: ${round(aveagechange,2)}")
+    print(f"Greatest Increase in Profits: {datetime.date.strftime(montincformat,'%b-%Y')} (${round(grtinc)})")
+    print(f"Greatest Decrease in Profits: {datetime.date.strftime(montdecformat,'%b-%Y')} (${round(grtdec)})")
     
